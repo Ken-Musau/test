afa9fc70543038bfe0559c5c0faea781e7fa69e8
Kennedy Musau <kennedy.musauu@gmail.com>
2023-11-07 19:31:03 +0300

    update working data

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const data = [
     publicationDate: "1965-01-01",
     author: "Frank Herbert",
     genres: ["science fiction", "novel", "adventure"],
-    hasMovieAdaptation: true,
+    hasMovieAdaptation: false,
     pages: 658,
     translations: {
       spanish: "",
@@ -142,14 +142,192 @@ function getBooks() {
 function getBook(id) {
   return data.find((d) => d.id === id);
 }
+/*
+const books = getBooks();
+console.log(books);
+
+const titles = books.map((book) => book.title);
+titles;
+
+// Map method
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+}));
+
+console.log(essentialData);
+
+// Filter method
+const logBooks = books.filter(
+  (book) => book.pages > 500 && book.hasMovieAdaptation
+);
+// .filter((book) => book.hasMovieAdaptation);
+logBooks;
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
+
+// Reduce method
+
+const pagesAllBooks = books.reduce((acc, book) => acc + book.pages, 0);
+pagesAllBooks;
+
+const people = [
+  { name: "Kennedy Musau", age: 70 },
+  { name: "Marge Lenana", age: 700 },
+  { name: "Emily Mbithe", age: 80 },
+];
+
+const totalAge = people.reduce((cur, person) => cur + person.age, 0);
+totalAge;
+
+const oldestAge = people.reduce(
+  (cur, person) => (person.age > cur ? person.age : cur),
+  0
+);
+oldestAge;
+
+const output = people.reduce((acc, cur, i, arr) => {
+  const split = cur.name.split(" ");
+
+  let part = `${split[0][0]}${split[1][0]}`;
+
+  i === arr.length - 1 ? (part += ".") : (part += ", ");
+  return acc + part;
+}, "");
+output;
+const split = "Kennedy Musau".split(" ");
+split;
+
+// Sort method
+const numbered = [2, 6, 12, 6, 8, 3, 1];
+const sorted = numbered.slice().sort((a, b) => b - a);
+sorted;
+numbered;
+
+const sortedByPages = books
+  .slice()
+  .sort((a, b) => b.pages - a.pages)
+  .map((book) => `${book.title} has ${book.pages}`);
+sortedByPages;
+
+// Working with immutable data
+
+//  1) adding a boook
+const newBook = {
+  id: 6,
+  title: "Harry Porter and the chamber of secrets",
+  author: "J. K. ROwling",
+};
+
+const booksAferAdd = [...books, newBook];
+
+booksAferAdd;
+
+//  2) Deleting a book
+const booksAfterDelete = booksAferAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3) Update book object in the array
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id === 1 ? { ...book, title: "kenya", pages: 2, read: "no" } : book
+);
+
+booksAfterUpdate;
+
 
 // destructuring
 
-const book = getBook(1);
+const book = getBook(3);
+console.log(book);
 
-const { title, author, genres } = book;
+// destructuring in objects
+const { genres, author, title, publicationDate, reviews } = book;
 console.log(author, title, genres);
 
-const [page2, page1] = genres;
-page1;
-page2;
+// destructuring in arrays and rest in arrays ...otherGenres
+const [primaryGenre, ...otherGenres] = genres;
+console.log(primaryGenre, otherGenres);
+
+// rest in objects
+const { pages, ...otherProperties } = book;
+otherProperties;
+
+// rest in arrays
+otherGenres;
+
+// Spread in arrays
+console.log([...genres, "water"]);
+
+// Spread in objects
+const updatedBook = {
+  ...book,
+  // add new property
+  moviePublicationDate: "2001",
+
+  // update existing property
+  pages: 20,
+};
+
+updatedBook;
+
+const getYear = (str) => str.split("-")[0];
+const summary = `${title}, is a  ${pages}-page long book, was written by ${author} and published in ${getYear(
+  publicationDate
+)}.`;
+summary;
+
+const pagesRange = pages > 1000 ? "Over a thousand" : "less than 1000";
+pagesRange;
+
+console.log(`The book has ${pagesRange} pages`);
+
+console.log(0 && "You are awesome");
+console.log(true || "You are awesome");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish ?? "Not translated";
+spanishTranslation;
+
+console.log(0 ?? "No data");
+console.log(null ?? "empty string");
+console.log(false ?? "empty string");
+console.log(!true && "some string");
+
+console.log(undefined ?? 0);
+
+console.log(reviews.goodreads.reviewsCount);
+// console.log(reviews.librarything.reviewsCount);
+
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews.goodreads.reviewsCount;
+  const librarything = book.reviews.librarything?.reviewsCount ?? 0;
+  return goodreads + librarything;
+}
+
+console.log(getTotalReviewCount(book));
+*/
+
+async function fetchData() {
+  try {
+    const resp = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await resp.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// fetchData();
+
+const todos = fetchData();
+console.log(todos);
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+console.log("jona");
